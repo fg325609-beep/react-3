1b0e5a8e640dc7d34fefb1016804ec0aae0dc199
Farhod Gofurov <fg325609@gmail.com>
2026-05-02 17:55:30 +0500

    jjjjj

--- src/companents/cards/Cards.jsx
+++ src/companents/cards/Cards.jsx
@@ -2,13 +2,11 @@ import React, { useState } from 'react';
 import "./Cards.scss";
 
 function Cards() {
-    // useState funksiya kabi () qavs bilan yozilishi shart
     const [count, setCount] = useState(0);
 
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>{count}</h1>
-            {/* setCount funksiyasini chaqirishda count, deb yozmang */}
             <button onClick={() => setCount(count + 1)}>Plus</button>
             <button onClick={() => setCount(0)}>Reset</button>
             <button onClick={() => setCount(count - 1)}>Minus</button>
