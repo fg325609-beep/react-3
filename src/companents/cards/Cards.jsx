4c4191b0fbb70ef0c66d024f56f7ea6001056edb
Farhod Gofurov <fg325609@gmail.com>
2026-05-02 17:59:00 +0500

    lllllllllllllll

--- src/companents/cards/Cards.jsx
+++ src/companents/cards/Cards.jsx
@@ -5,7 +5,7 @@ function Cards() {
     const [count, setCount] = useState(0);
 
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <div>
             <h1>{count}</h1>
             <button onClick={() => setCount(count + 1)}>Plus</button>
             <button onClick={() => setCount(0)}>Reset</button>
